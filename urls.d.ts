36b47126074dae1b5142c91b7b8bd5c16e6d0733
cardinalby <cardinalby@gmail.com>
2023-09-19 20:52:34 +0000

    Deploying to gh-pages from @ cardinalby/aquarelaks-schedule@57b577dcd85e190a4feef1b82e951651ee2cbedc 🚀

--- urls.d.ts
+++ urls.d.ts
@@ -1,4 +1,3 @@
-export declare const CORS_PROXY_URL = "https://aquarelaks.cardinalby.workers.dev/";
 export declare const AQUARELAKS_URL = "https://sport.um.warszawa.pl/waw/ucsir/pod-strzecha-3";
 export declare const AQUARELAKS_LINKS_BASE_URL = "https://sport.um.warszawa.pl";
 export declare function getProxiedUrl(destUrl: string): string;
--- urls.js
+++ urls.js
@@ -1,4 +1,3 @@
-export const CORS_PROXY_URL = "https://aquarelaks.cardinalby.workers.dev/";
 export const AQUARELAKS_URL = "https://sport.um.warszawa.pl/waw/ucsir/pod-strzecha-3";
 export const AQUARELAKS_LINKS_BASE_URL = "https://sport.um.warszawa.pl";
 export function getProxiedUrl(destUrl) {
